baec57ab3eede0f01b782c9e885ac159b634bc71
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-07-14 10:34:35 +0800

    default stdin content;reload;README

---
+++ README.md
@@ -0,0 +1,55 @@
+# openconnect wrapper
+
+## Objective
+
+Every time I connect to Duke VPN using openconnect,
+I need to type in the `<GROUP>`,
+`<USERNAME>`,
+and `<PASSWORD>`
+manually,
+which is annoying.
+openconnect wrapper does those for me.
+
+## Overview
+
+openconnect wrapper runs
+
+```shell
+openconnect <URL_TO_CONNECT_TO>
+```
+
+and fill in
+
+```text
+<GROUP>
+<USERNAME>
+<PASSWORD>
+
+```
+
+into the process's stdin
+so I don't have to type all the boilerplate every time.
+
+## Usage
+
+1. Change the `<URL_TO_CONNECT_TO>`,
+    `<GROUP>`,
+    `<USERNAME>`,
+    and `<PASSWORD>`
+    to your use case in `src/main.rs`.
+1. Run the binary with `sudo` so openconnect can work:
+
+   ```shell
+   sudo cargo r --release
+   ```
+
+1. Type in the Response to the MFA option and press <kbd>enter</kbd>.
+1. If you want to drop the connection and reconnect,
+    press <kbd>r</kbd> and then <kbd>enter</kbd> so it reconnects.
+1. To exit, press <kbd>ctrl</kbd> <kbd>c</kbd>.
+
+## Compatibility
+
+I use macOS and this is where openconnect wrapper is tested.
+I'm confident that it would just work on Linux as well.
+There is no way that this world work on Windows without modifications.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,45 @@
-use std::{error::Error, process::Command};
+use std::{
+    io::{stdin, Result, Write},
+    process::{Child, ChildStdin, Command, Stdio},
+};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut process = Command::new("openconnect").arg("portal.duke.edu").spawn()?;
-    process.wait()?;
-    Ok(())
+/// Use openconnect to connect to URL specified in `create_process`
+/// and fill in information specified in `get_process_stdin` automatically.
+///
+/// Listen to stdin
+/// and filter out input invalid to openconnect.
+///
+/// Press `r` + `enter` to reconnect.
+fn main() -> Result<()> {
+    let mut process = create_process()?;
+    let mut process_stdin = get_process_stdin(&mut process)?;
+    let stdin = stdin();
+    loop {
+        let mut buf = String::new();
+        stdin.read_line(&mut buf)?;
+        if buf == "r\n" {
+            process.kill()?;
+            process = create_process()?;
+            process_stdin = get_process_stdin(&mut process)?;
+        } else if buf.trim().chars().all(|c| c.is_numeric()) {
+            process_stdin.write_all(buf.as_bytes())?;
+        } else {
+            eprintln!("[WRAPPER] input `{buf}` is not numeric")
+        }
+    }
+}
+
+/// Run `openconnect <URL_TO_CONNECT_TO>`.
+fn create_process() -> Result<Child> {
+    Command::new("openconnect")
+        .arg("<URL_TO_CONNECT_TO>")
+        .stdin(Stdio::piped())
+        .spawn()
+}
+
+/// Fill in <GROUP>, <USERNAME>, and <PASSWORD> to openconnect's stdin.
+fn get_process_stdin(process: &mut Child) -> Result<ChildStdin> {
+    let mut process_stdin = process.stdin.take().unwrap();
+    process_stdin.write_all(b"<GROUP>\n<USERNAME>\n<PASSWORD>\n")?;
+    Ok(process_stdin)
 }
